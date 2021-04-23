1ded6ee11e4d0a89626bf362f3c3445c56f28394
Adam Tyll <daisybell.dev@gmail.com>
2021-04-23 15:13:48 +0200

    debug passing user attr

--- routes/User/index.js
+++ routes/User/index.js
@@ -61,6 +61,7 @@ router.get('/list', auth, async (req, res) => {
 // USER AUTHENTICATION ROUTE
 router.get('/auth', auth, async (req, res) => {
     try {
+        console.log({ user: req.user });
         const { email, password } = req.user;
         const user = await UserModel.findByCredentials(email, password);
         if (!user) {
